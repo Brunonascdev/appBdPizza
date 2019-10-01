0891126511fd89b8d051c06327fd61907a8bbeff
Brunonascdev <brunonascimentodev@gmail.com>
2019-10-02 03:37:08 +0900

    Cadastro no BD com sucesso.

--- www/components/script.js
+++ www/components/script.js
@@ -1,3 +1,24 @@
+$(document).on("click", "#register", function(){
+  var parametros = {
+    "sabor":$("#nome").val(),
+    "valor":$("#valor").val()
+  };
 
+  $.ajax({
+    type:"post", // Como vai enviar os dados
+    url:"https://appbdpizzaria.000webhostapp.com/cadastro.php", // pra onde vai enviar
+    data:parametros, // o que eu vou enviar
+    // caso sucesso
+    success: function(data){ 
+      navigator.notification.alert(data);
+      $("#nome").val("")
+      $("#valor").val("")
+    },
+    // caso erro
+    error: function(data){
+      navigator.notification.alert("Erro ao cadastrar!");
+    }
+  });
+});
 
 
