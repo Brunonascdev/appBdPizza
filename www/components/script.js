bf3c5608e7278bf95dd68cfe99c0a21ca6e7a0ba
Brunonascdev <brunonascimentodev@gmail.com>
2019-10-04 04:56:53 +0900

    Mostrar código, sabor e preço em busca nas pizzas já registradas\;

--- www/components/script.js
+++ www/components/script.js
@@ -30,7 +30,7 @@ function carregaLista(){
     success: function(data){ 
       var itemlista = "";
       $.each(data.pizzas, function(i,dados){
-        itemlista += "<option value="+dados.codigo+">"+dados.sabor+"</option>"
+        itemlista += "<option value="+dados.codigo+" title"+dados.sabor+" alt"+dados.valor+">"+dados.sabor+"</option>"
       });
       $("#selectListas").html(itemlista);
     },
@@ -40,5 +40,43 @@ function carregaLista(){
     }
   });
 }
+
+$(document).on("click", "#registerPizza", function(){
+  $("#codigo").removeAttr('readonly');
+  $("#sabor").removeAttr('readonly');
+  $("#preço").removeAttr('readonly');
+});
+
+ $(document).ready(function (){
+    carregaLista();
+        $.ajax({
+    type:"post", // Como vai enviar os dados
+    url:"https://appbdpizzaria.000webhostapp.com/listar.php", // pra onde vai enviar
+    dataType:"json", // o que eu vou enviar
+    // caso sucesso
+    success: function(data){ 
+    $( ".listas" ).change(function() {
+      var cd = $("select option:selected").val();
+      $("#codigo").val(cd);
+      var sabor = $("select option:selected").text();
+      $("#sabor").val(sabor);
+      var preço = '';
+      $.each(data.pizzas, function(i,dados){
+        if (cd == dados.codigo){
+          preço = this.valor;
+        }
+      });
+      $("#preço").val(preço);
+    });
+    },
+    // caso erro
+    error: function(data){
+      navigator.notification.alert("Erro ao buscar registros!");
+    }
+  });
+    
+});
+
+
 
 
