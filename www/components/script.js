babf88e6e28674b1d993893f83f754c1aafd3b70
Brunonascdev <brunonascimentodev@gmail.com>
2019-10-04 03:32:40 +0900

    Função de mostrar pizzas registradas foi criado\;

--- www/components/script.js
+++ www/components/script.js
@@ -21,4 +21,24 @@ $(document).on("click", "#register", function(){
   });
 });
 
+function carregaLista(){
+    $.ajax({
+    type:"post", // Como vai enviar os dados
+    url:"https://appbdpizzaria.000webhostapp.com/listar.php", // pra onde vai enviar
+    dataType:"json", // o que eu vou enviar
+    // caso sucesso
+    success: function(data){ 
+      var itemlista = "";
+      $.each(data.pizzas, function(i,dados){
+        itemlista += "<option value="+dados.codigo+">"+dados.sabor+"</option>"
+      });
+      $("#selectListas").html(itemlista);
+    },
+    // caso erro
+    error: function(data){
+      navigator.notification.alert("Erro ao buscar registros!");
+    }
+  });
+}
+
 
